96ca13eb626d1b34163692c762222f969c9d1114
Soumya-RanjanBhoi <soumya6ranjan9@gmail.com>
2026-03-25 23:14:22 +0530

    modified req.txt

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from src.database import crud
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
